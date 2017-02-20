d48faaa32a3294af16290fa09f6f81562cdacf7a
Anirudha Mahale <anirudhamahale@gmail.com>
2017-02-20 14:46:31 +0530

    Simple Gradient Layer is done.

--- CATransformLayer/ViewController.swift
+++ CATransformLayer/ViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        draw(Gradient: containerView)
     }
     
+    func draw(Gradient view: UIView) {
+        // create gradient layer and add it to the view
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        view.layer.addSublayer(gradientLayer)
+        
+        // set gradient layer colors
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        
+        // set the gradient start and end point
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+    }
 }
 
