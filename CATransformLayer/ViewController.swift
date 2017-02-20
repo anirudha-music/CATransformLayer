f6bd905a6cc11fcb4aa371761d967497da61d45d
Anirudha Mahale <anirudhamahale@gmail.com>
2017-02-20 14:52:23 +0530

    CAGradientLayer is done.

--- CATransformLayer/ViewController.swift
+++ CATransformLayer/ViewController.swift
@@ -24,11 +24,21 @@ class ViewController: UIViewController {
         view.layer.addSublayer(gradientLayer)
         
         // set gradient layer colors
-        gradientLayer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor]
+        
+        // set the locations
+        gradientLayer.locations = [0.0, 0.2, 0.5]
         
         // set the gradient start and end point
         gradientLayer.startPoint = CGPoint(x: 0, y: 0)
         gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+    }
+    
+    func draw(multipartGradient view: UIView) {
+        // create gradient layer and add it to the view
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        view.layer.addSublayer(gradientLayer)
     }
 }
 
